6c5da765980f86b1382c9d5d761f02262f5c799c
alessiaBer <alexdeyn@gmail.com>
2023-03-14 15:37:23 +0100

    [add] prev and next img buttons features

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,6 +27,7 @@ const carouselEl = document.querySelector('.carousel');
 
 //creo una variabile per l'img attiva settata all'index 0
 let activeImg = 0;
+
 // creo un loop che passi ogni immagine dell'array 
 for (let i = 0; i < imgArray.length; i++) {
     //assegno ad una variabile ogni immagine dell'array usando l'index 
@@ -34,9 +35,49 @@ for (let i = 0; i < imgArray.length; i++) {
     //creo l'elemento HTML per l'img e lo assegno ad una variabile, l'elemento avrà una classe dinamica e il src in base all'index dell'array
     const imgEl = `<img class="img ${i === activeImg ? 'active' : ''}" src="${images}">`;
     //inserisco l'elemento HTML per l'img nel .carousel presente nell'html
-    carouselEl.insertAdjacentHTML('afterbegin', imgEl);
+    carouselEl.insertAdjacentHTML('beforeend', imgEl);
 }
 
 /**** MILESTONE 3
 Al click dell'utente sulle frecce, 
-il programma cambierà l’immagine attiva, che quindi verrà visualizzata al posto della precedente.*/
+il programma cambierà l’immagine attiva, 
+che quindi verrà visualizzata al posto della precedente.*/
+
+//seleziono tutte le immagini create nel DOM e le assegno ad una variabile
+const allImgEl = document.querySelectorAll('.carousel > .img');
+//seleziono dal DOM il btn .next
+const nextBtn = document.querySelector('.next');
+//seleziono dal DOM il btn .prev
+const prevBtn = document.querySelector('.prev');
+
+
+//assegno al btn .next un eventListener per il click
+nextBtn.addEventListener('click', function() {
+    //seleziono l'indice dell'img attiva e la assegno ad una variabile
+    const currentImg = allImgEl[activeImg];
+    console.log(currentImg)
+    //rimuovo dall'img corrente la class active
+    currentImg.classList.remove('active');
+    //incremento il valore dell'index dell'activeImg
+    activeImg++;
+    //seleziono l'img successiva e la assegno ad una variabile
+    const nextImg = allImgEl[activeImg];
+    //aggiungo all'immagine successiva la classe active
+    nextImg.classList.add('active');
+})
+
+//assegno al btn .prev un eventListener per il click
+prevBtn.addEventListener('click', function() {
+    //seleziono l'indice dell'img attiva e la assegno ad una variabile
+    const currentImg = allImgEl[activeImg];
+    console.log(currentImg)
+    //rimuovo dall'img corrente la class active
+    currentImg.classList.remove('active');
+    //decremento il valore dell'index dell'activeImg
+    activeImg--;
+    //seleziono l'img precedente e la assegno ad una variabile
+    const prevImg = allImgEl[activeImg];
+    //aggiungo all'immagine successiva la classe active
+    prevImg.classList.add('active');
+})
+
