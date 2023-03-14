0a7d1965249a0e3d642da01fa1d0f05bf08fc8c3
alessiaBer <alexdeyn@gmail.com>
2023-03-14 14:41:11 +0100

    [add] dynamically images (milestone 2)

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,6 +13,29 @@ Tutte le immagini saranno nascoste, tranne la prima, che avrà una classe specif
 Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella milestone 1, 
 ma costruito dinamicamente attraverso JavaScript.*/
 
+//creo l'array delle immagini
+const imgArray = [
+    './assets/img/01.webp',
+    './assets/img/02.webp',
+    './assets/img/03.webp',
+    './assets/img/04.webp',
+    './assets/img/05.webp'
+];
+
+//seleziono l'elemento .carousel dal DOM e lo assegno ad una variabile
+const carouselEl = document.querySelector('.carousel');
+
+//creo una variabile per l'img attiva settata all'index 0
+let activeImg = 0;
+// creo un loop che passi ogni immagine dell'array 
+for (let i = 0; i < imgArray.length; i++) {
+    //assegno ad una variabile ogni immagine dell'array usando l'index 
+    const images = imgArray[i];
+    //creo l'elemento HTML per l'img e lo assegno ad una variabile, l'elemento avrà una classe dinamica e il src in base all'index dell'array
+    const imgEl = `<img class="img ${i === activeImg ? 'active' : ''}" src="${images}">`;
+    //inserisco l'elemento HTML per l'img nel .carousel presente nell'html
+    carouselEl.insertAdjacentHTML('beforeend', imgEl);
+}
 
 /**** MILESTONE 3
 Al click dell'utente sulle frecce, 
